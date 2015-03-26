61832286512188fb66f34c9ce076d0d3e713ffe4
FusionDevil <fusdevil@gmail.com>
2015-03-27 01:00:18 +0700

    Added namtth folder

---
+++ namtth/tut2/src/tut2/Tut2.java
@@ -0,0 +1,11 @@
+package tut2;
+
+public class Tut2
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Enter something");
+        String line = TextIO.getln();
+        System.out.println("You entered: " + line);
+    }
+}   
